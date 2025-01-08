4315bf0d8bb224d1e7dd7a26759bbaec7fc56387
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2025-01-08 14:53:50 +0100

    Forelesning1 og litt mer å øve på :)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Opprettet et objekt "Rania" av Type "Person" ved bruk av nøkkel ord "new" og konstruktøren "Person()"
+Person Rania = new Person();
+
+// Setter verdi til "Navn" som er automatisk implementert engenskap
+Rania.Navn = "Rania";
+
+// Kaller "Spise()" og "Kjøpe()" metodene på "Rania" objektet
+Rania.Spise();
+Rania.Kjøpe();
