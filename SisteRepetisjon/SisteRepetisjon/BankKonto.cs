d78d610de583f393e6b18b2d744eb1a52bba53cd
RaniaEG <79601775+RaniaEG@users.noreply.github.com>
2025-05-01 16:18:31 +0200

    Siste Repetisjon

---
+++ SisteRepetisjon/SisteRepetisjon/BankKonto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisteRepetisjon
+{
+    internal class BankKonto
+    {
+        public readonly string kontoNummer;
+        public decimal Saldo { get; set; } /// vi kan sette Saldo som private variabel (i klassen) 
+        // Eller kan sette Saldo som readonly (i konstruktøren)
+        // public Beløp TaUtPenger(decimal antall){   }
+        // public Beløp SettInnPenger(decimal antall){     }
+
+        public BankKonto(string kontonummer)
+        {
+            Saldo = 0;
+            kontoNummer = kontonummer;
+        }
+
+        /* Hvis begge variablene har samme navn, for å unngå semantisk feil i programmet så bruker vi "this"
+         * public BankKonto(string kontoNummer)
+        {
+            Saldo = 0;
+            this.kontoNummer = kontoNummer;
+        }
+
+        // Det skal fungere greit siden Saldo som egenskap starter med stor bokstav 
+        // og saldo som parameter starter med liten bokstav
+        // Dette her er et eksempel på konstruktør overloading (samme signatur men forskjellig antall parametere
+        public BankKonto(string kontoNummer, decimal saldo)
+        {
+            Saldo = saldo;
+            this.kontoNummer = kontoNummer;
+        }
+         */
+
+        public BankKonto(string kontoNummer, decimal saldo)
+        {
+            Saldo = saldo;
+            this.kontoNummer = kontoNummer;
+        }
+    }
+}
---
+++ SisteRepetisjon/SisteRepetisjon/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using SisteRepetisjon;
+
+// Dette her er konstruktør overloading men ikke polymorfisme (polymorfisme trenger arv)
+var bankkonto = new BankKonto("1234");
+var bankkont2 = new BankKonto("1234", 1000);
+
+
+// 
